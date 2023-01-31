538a1038172ceee1396af1fc4a6e1a3c86ee17f5
Raj <rajlilhare200@gmail.com>
2023-01-31 02:01:48 -0800

    asdfg

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import express  from "express";
-import dotenv from "dotenv"
+import express from "express";
+import dotenv from "dotenv";
 
 import router from "./routes/courseRoute.js";
 import user from "./routes/userRoute.js";
@@ -10,46 +10,47 @@ import otherroute from "./routes/otherRoutes.js";
 
 // agar frontend backend ko connect karna
 //  hia to cors ko use karna padega
-import cors from "cors"
+import cors from "cors";
 
 dotenv.config({
-    path:"./config/.env"
-})
+  path: "./config/.env",
+});
 
-const app=express()
-
+const app = express();
 
 // using middlewraes
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(express.urlencoded({
-    extended:true
-}))
-
-
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 // app.use(cors())
 
-app.use(cookieParser())
-// to cors me kuch mandatory chize deni hai 
+app.use(cookieParser());
+// to cors me kuch mandatory chize deni hai
 // otherwise hum cookie transfer nahi kar payge
-app.use(cors({
-    origin:'https://my-startup-frontend.vercel.app',
-    credentials:true,
-    method:["GET","POST","PUT","DELETE"]
-}))
-
+app.use(
+  cors({
+    origin: "https://my-startup-frontend.vercel.app/",
+    credentials: true,
+    method: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
 // importing routes
-app.use(router)
-app.use(user)
-app.use(payrouter)
-app.use(otherroute)
-app.get("/",(req,res)=>{
-    res.send(`<h1>here is the <a href=${process.env.FRONTENDURL} >link</a> for frontend `)
-})
- export default app
+app.use(router);
+app.use(user);
+app.use(payrouter);
+app.use(otherroute);
+app.get("/", (req, res) => {
+  res.send(
+    `<h1>here is the <a href=${process.env.FRONTENDURL} >link</a> for frontend `
+  );
+});
+export default app;
 
-
- app.use(errorMiddleware)
+app.use(errorMiddleware);
